6623c6691e130b3cff45df3e33ea2fccefbc104a
Nikolay <nikolay.topalov@gmail.com>
2021-02-16 17:46:47 +0200

    animation added to review component

--- src/components/Review.vue
+++ src/components/Review.vue
@@ -2,13 +2,18 @@
   <button class="iconBtn" @click="toggleReview()">
      <i class="large material-icons">star_border</i>
   </button>
+  <transition name="reviews">
     <template v-if="isVissible">
-      <h4>Reviews</h4>
-      <ul v-if="reviews.reviews.length">
-        <li v-for="review in reviews.reviews" :key="review._id">{{review.author.username}} : {{review.review}}</li>
-      </ul>
-      <p v-else>No reviews yet</p>
+      <div class="review">
+        <h4>Reviews</h4>
+        <ul v-if="reviews.reviews.length">
+          <li v-for="review in reviews.reviews" :key="review._id">{{review.author.username}} : {{review.review}}</li>
+        </ul>
+        <p v-else>No reviews yet</p>
+        <button class="iconBtn" @click="toggleReview"><i class="material-icons">close</i></button>
+      </div>
     </template>
+  </transition>
 </template>
 <script lang="ts">
 import store from "@/store";
@@ -23,7 +28,6 @@ export default class Review extends Vue.with(Props) {
   isVissible = false;
   get reviews() {
     const reviews = store.state.reviews.find(r => r.recipeId === this.recipeId);
-    console.log(reviews)
     return reviews;
   }
   toggleReview() {
@@ -32,5 +36,28 @@ export default class Review extends Vue.with(Props) {
 }
 </script>
 <style lang="stylus" scoped>
-
+.reviews-enter-from
+  transform translateX(100%)
+.reviews-enter-to
+  transform translateX(0%)
+.reviews-enter-active
+  transition  all 1s ease
+.reviews-leave-from
+  transform translateX(0%)
+.reviews-leave-to
+  transform translate(100%)
+.reviews-leave-active
+  transition all 1s ease
+.review
+  position absolute
+  top 0
+  right 0
+  overflow-y auto
+  width 60%
+  height 90%
+  max-width 450px
+  margin 0 auto
+  padding 26px
+  background-color #e5e4e2
+  display grid
 </style>
