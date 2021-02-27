abeea22f9edc252bb6b13094165d9006fc09d27e
Nikolay <nikolay.topalov@gmail.com>
2021-02-27 10:58:03 +0200

    recipe-create component & recipe form migrated to js

--- src/views/Create-recipe.vue
+++ src/views/Create-recipe.vue
@@ -1,52 +1,52 @@
 <template>
-  <ValidationInfo :fields="props" />
+  <ValidationInfo :fields="requarements" />
   <h1>Share your tasty ideas with others!</h1>
   <RecipeForm @handleSubmit="registerRecipe" />
 </template>
+<script>
+import RecipeForm from '@/components/Recipe-form.vue';
+import ValidationInfo from '@/components/Validation-info.vue';
+import { useStore } from 'vuex';
+export default {
+  components: { RecipeForm, ValidationInfo },
+  setup() {
+    const store = useStore();
+    const requarements = [
+      {
+        name: 'Name',
+        reqs: [
+          'min 4 characters',
+          'max 20 characters',
+          'Only english letters and/or numbers',
+          'No spaces,special characters allowed',
+        ],
+      },
+      {
+        name: 'Description',
+        reqs: ['min 20 characters', 'max 200 characters'],
+      },
+      { name: 'Cover link', reqs: ['starting with http:// or https://'] },
+      {
+        name: 'Ingrediants',
+        reqs: ['Quantity - only numbers', 'gr/kg/ml or empty for pieces'],
+      },
+      {
+        name: 'Type',
+        reqs: [
+          'Public - everyone can read it',
+          'Private - only you can see it',
+        ],
+      },
+    ];
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import RecipeForm from "@/components/Recipe-form.vue";
-import ValidationInfo from "@/components/Validation-info.vue";
-import { Recipe } from "@/core/models";
-import store from "@/store";
-@Options({
-  components: {
-    RecipeForm,
-    ValidationInfo
-  }
-})
-export default class CreateRecipe extends Vue {
-  private requarements = [
-    {
-      name: "Name",
-      reqs: [
-        "min 4 characters",
-        "max 20 characters",
-        "Only english letters and/or numbers",
-        "No spaces,special characters allowed"
-      ]
-    },
-    { name: "Description", reqs: ["min 20 characters", "max 200 characters"] },
-    { name: "Cover link", reqs: ["starting with http:// or https://"] },
-    {
-      name: "Ingrediants",
-      reqs: ["Quantity - only numbers", "gr/kg/ml or empty for pieces"]
-    },
-    {
-      name: "Type",
-      reqs: ["Public - everyone can read it", "Private - only you can see it"]
-    }
-  ];
-  public get props() {
-    return this.requarements;
-  }
-  registerRecipe(payload: Recipe) {
-    store.dispatch("recipe/create", payload);
-    this.$router.push("/");
-  }
-}
+    const registerRecipe = (payload) => {
+      store.dispatch('recipe/create', payload);
+    };
+    return { requarements, registerRecipe };
+  },
+};
 </script>
+
 <style lang="stylus" scoped>
 h1
   margin-top 20px
