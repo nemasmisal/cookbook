a75f5e9df7339c3669a3626e53f8075f516bb63f
Nikolay <nikolay.topalov@gmail.com>
2021-02-28 17:33:26 +0200

    vuelidate validations added to recipe-form component

--- src/components/Register-form.vue
+++ src/components/Register-form.vue
@@ -4,9 +4,12 @@
     <input
       type="text"
       name="name"
-      placeholder="Currly brocolli?"
-      v-model.lazy="form.name"
-      :class="{ error: form.errors.name() }"
+      placeholder="Currly broccoli?"
+      v-model="v$.form.name.$model"
+    />
+    <InputErrMsgTemp
+      :errorsObj="v$.form.name.$silentErrors"
+      v-if="v$.form.name.$dirty"
     />
     <label for="email">E-Mail</label>
     <input
@@ -14,58 +17,75 @@
       name="email"
       id="email"
       placeholder="email@example.com"
-      v-model="form.email"
-      :class="{ error: form.errors.email() }"
+      v-model="v$.form.email.$model"
+    />
+    <InputErrMsgTemp
+      :errorsObj="v$.form.email.$silentErrors"
+      v-if="v$.form.email.$dirty"
     />
     <label for="password">Password</label>
     <input
       type="password"
       name="password"
       id="password"
-      v-model="form.password"
-      :class="{ error: form.errors.password() }"
+      v-model="v$.form.password.$model"
+    />
+    <InputErrMsgTemp
+      :errorsObj="v$.form.password.$silentErrors"
+      v-if="v$.form.password.$dirty"
     />
     <label for="repeatPassword">Repeat password</label>
     <input
       type="password"
       name="repeatPassword"
       id="repeatPassword"
-      v-model="form.repeatPassword"
-      :class="{ error: form.errors.repeatPassword() }"
+      v-model="v$.form.repeatPassword.$model"
     />
-    <button type="submit">Create an Account</button>
+    <InputErrMsgTemp
+      :errorsObj="v$.form.repeatPassword.$silentErrors"
+      v-if="v$.form.repeatPassword.$dirty"
+    />
+    <button type="submit" :disabled="v$.invalid">Create an Account</button>
   </form>
 </template>
 <script>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
+import useVuelidate from '@vuelidate/core';
+import InputErrMsgTemp from '@/components/Input-err-msg-temp.vue';
+import {
+  required,
+  email,
+  maxLength,
+  minLength,
+  helpers,
+  sameAs,
+} from '@vuelidate/validators';
 export default {
+  components: { InputErrMsgTemp },
   setup() {
     const store = useStore();
-    const patterns = {
-      oneWorld: new RegExp(/^[a-zA-Z0-9]{4,20}$/),
-      email: new RegExp(/^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.{1}[a-zA-Z]+$/),
-    };
-    const form = ref({
-      name: '',
-      email: '',
-      password: '',
-      repeatPassword: '',
-      errors: {
-        name: () => !patterns.oneWorld.test(form.value.name),
-        email: () => !patterns.email.test(form.value.email),
-        password: () => !patterns.oneWorld.test(form.value.password),
-        repeatPassword: () =>
-          form.value.password !== form.value.repeatPassword ||
-          form.value.repeatPassword.length === 0,
+    const form = ref({ name: '', email: '', password: '', repeatPassword: '' });
+    const oneWordPattern = helpers.withMessage(
+      'Allowed characters: A-Z,0-9',
+      (value) => /^[a-zA-Z0-9]+$/.test(value)
+    );
+    const rules = computed(() => ({
+      form: {
+        name: { required, oneWordPattern },
+        email: { required, email },
+        password: {
+          required,
+          maxLength: maxLength(20),
+          minLength: minLength(4),
+          oneWordPattern,
+        },
+        repeatPassword: { sameAs: sameAs(form.value.password), required },
       },
-    });
-
+    }));
+    const v$ = useVuelidate(rules, { form });
     const handleSubmit = async () => {
-      const isInvalid = Object.values(form.value.errors).find((f) => f());
-      if (isInvalid) {
-        return;
-      }
+      if (v$.value.$invalid) return;
       const credentials = {
         username: form.value.name,
         email: form.value.email,
@@ -73,7 +93,7 @@ export default {
       };
       store.dispatch('register', credentials);
     };
-    return { handleSubmit, form };
+    return { handleSubmit, form, v$ };
   },
 };
 </script>
