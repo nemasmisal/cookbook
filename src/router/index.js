ae9adcecd7fb91e7a86d5caa1e96bc6c0bb3618e
Nikolay <nikolay.topalov@gmail.com>
2021-03-04 13:02:30 +0200

    router guards added

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,59 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import store from '../store';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Home',
+    component: Home,
   },
   {
-    path: "/login",
-    name: "Login",
-    component: () => import("../views/Auth/Login.vue")
+    path: '/login',
+    name: 'Login',
+    beforeEnter: (to, from, next) => {
+      if (store.state.auth.id) return next({ path: '/' });
+      next();
+    },
+    component: () => import('../views/Auth/Login.vue'),
   },
   {
-    path: "/register",
-    name: "Register",
-    component: () => import("../views/Auth/Register.vue")
+    path: '/register',
+    name: 'Register',
+    beforeEnter: (to, from, next) => {
+      if (store.state.auth.id) return next({ path: '/' });
+      next();
+    },
+    component: () => import('../views/Auth/Register.vue'),
   },
   {
-    path: "/create",
-    name: "Create-recipe",
-    component: () => import("../views/Recipe/Create-recipe.vue")
+    path: '/create',
+    name: 'Create-recipe',
+    beforeEnter: (to, from, next) => {
+      if (store.state.auth.id) return next();
+      next({ path: '/login' });
+    },
+    component: () => import('../views/Recipe/Create-recipe.vue'),
   },
   {
-    path: "/about",
-    name: "About",
-    component: () => import("../views/About.vue")
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue'),
   },
   {
-    path: "/edit/:id",
-    name: "Edit-recipe",
-    component: () => import("../views/Recipe/Edit-recipe.vue")
-  }
+    path: '/edit/:id',
+    name: 'Edit-recipe',
+    beforeEnter: (to, from, next) => {
+      if (store.state.auth.id) return next();
+      next({ path: '/login' });
+    },
+    component: () => import('../views/Recipe/Edit-recipe.vue'),
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
