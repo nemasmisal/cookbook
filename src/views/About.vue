20830528abf22f5a7cd29b8e90254ca79929de37
Nikolay <nikolay.topalov@gmail.com>
2021-03-12 11:59:25 +0200

    about page added

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      This is my project defence for the VueJS course @ SoftUni University and
+      has no commercial purpose.
+    </h1>
+    <h4>
+      Here is the link of the repo @
+      <a href="https://github.com/nemasmisal/cookbook">Github</a>, where you can
+      find the code too.
+    </h4>
+    <div>
+      <h5>Technologies i've been using:</h5>
+      <ul>
+        <li>Vue 3.0</li>
+        <li>Vue Router 4.0</li>
+        <li>Vuex 4.0</li>
+        <li>Vuelidate/core 2.0 alpha.14</li>
+        <li>Vuelidate/validators 2.0 alpha.12</li>
+        <li>Core-Js 3.6</li>
+        <li>Stylus</li>
+      </ul>
+    </div>
   </div>
 </template>
+<style lang="stylus" scoped>
+a
+  color #ff6347
+h1
+  margin 20px auto
+h4,
+h5
+  margin 10px auto
+  padding 10px
+h5
+  text-align start
+  margin-left 30px
+li
+  list-style decimal
+  text-align start
+  margin-left 60px
+  padding 2px
+</style>
